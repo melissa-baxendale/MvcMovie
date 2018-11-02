5329795dbc7f0d047c5dcd2b1bcc3920cb55f7c3
Melissa Baxendale <melissa.baxendale@outlook.com>
2018-11-01 19:28:39 -0700

    Added Search capability by genre or name.

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcMovie.Models
 {
@@ -10,9 +11,13 @@ namespace MvcMovie.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
